158c10b619af7318a5dab9e46474264850e8d153
cyclicgraph <cyclicgraph@op.pl>
2023-12-12 22:56:24 +0100

    Add base of project

---
+++ src/main/java/com/cyclicgraph/masterymath/MasteryMathApplication.java
@@ -0,0 +1,13 @@
+package com.cyclicgraph.masterymath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteryMathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasteryMathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyclicgraph/masterymath/MasteryMathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyclicgraph.masterymath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasteryMathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
